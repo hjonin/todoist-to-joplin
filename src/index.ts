74b5d2a473002ee34e2d559134bc1e65d0e57203
hjonin <helene.jonin@data.gouv.fr>
2023-03-31 15:42:33 +0200

    set key through config screen instead of file

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,10 @@
 import joplin from 'api';
-import {MenuItemLocation} from "../api/types";
+import {MenuItemLocation, SettingItemType} from "../api/types";
 
-const fs = joplin.require('fs-extra');
 const ttm = require('todoist-to-md');
 
 const TODOIST_FOLDER_NAME = 'TODO-IST';
-
-const getSettings = async () => {
-    const installDir = await joplin.plugins.installationDir();
-    return JSON.parse(fs.readFileSync(installDir + '/settings.json', 'utf8'));
-}
+const ERROR_MESSAGE = '[Todoist to Joplin] Could not import data';
 
 const getOrCreateTodoFolder = async () => {
     let todoFolder;
@@ -20,11 +15,26 @@ const getOrCreateTodoFolder = async () => {
         todoFolder = todoFolderSearchRes.items[0];
     }
     return todoFolder;
+
 }
 
 joplin.plugins.register({
     onStart: async function () {
-        const settings = await getSettings();
+        await joplin.settings.registerSection('ttmSection', {
+            label: 'Todoist to Joplin',
+            iconName: 'fas fa-list',
+        });
+
+        await joplin.settings.registerSettings({
+            'ttmKeySetting': {
+                value: '',
+                type: SettingItemType.String,
+                section: 'ttmSection',
+                public: true,
+                secure: false,
+                label: 'Todoist API token',
+            },
+        });
 
         await joplin.commands.register({
             name: 'ttmCommand',
@@ -32,15 +42,22 @@ joplin.plugins.register({
             iconName: 'fas fa-list',
             execute: async () => {
                 let todoFolder = await getOrCreateTodoFolder();
-                ttm.todoistToMarkdown(settings.key).then((projects: any[]) => {
-                    for (const project of projects) {
-                        joplin.data.post(['notes'], null, {
-                            body: project.project_tree,
-                            title: project.project_name,
-                            parent_id: todoFolder.id
-                        }); // TODO merge duplicate notes
-                    }
-                });
+                const key = await joplin.settings.value('ttmKeySetting');
+                if (!key) {
+                    console.error('${ERROR_MESSAGE}: missing API token.');
+                } else {
+                    ttm.todoistToMarkdown(key).then((projects: any[]) => {
+                        for (const project of projects) {
+                            joplin.data.post(['notes'], null, {
+                                body: project.project_tree,
+                                title: project.project_name,
+                                parent_id: todoFolder.id
+                            }); // TODO merge duplicate notes
+                        }
+                    }).catch(error => {
+                        console.error(`${ERROR_MESSAGE}.`);
+                    });
+                }
             },
         });
 
